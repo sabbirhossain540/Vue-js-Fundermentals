f2afe3ed000c4b94d95cd8a012559b6556105d77
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-05 18:52:20 +0600

    Important topics Vue Props

---
+++ vue-practice/src/components/AllFriends.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="allfriends">
+		<h2>All Friends</h2>
+
+		<div v-for="(friend,index) in friends" :key="index" >
+			<h4> {{ friend.name }} </h4>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+
+	export default {
+		name: 'AllFriends',
+		props:['friends'],
+		data(){
+			return {
+				
+			}
+		}
+	}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-practice/src/components/OnlineFriends.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="onlinefriends">
+		<h2>Online Friends</h2>
+		<div v-for="(friend,index) in friends" :key="index" >
+			<h4 v-if="friend.online"> {{ friend.name }} </h4>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+
+	export default {
+		name: 'OnlineFriends',
+		props:['friends'],
+		data(){
+			return {
+				
+			}
+		}
+	}
+
+</script>
+
+<style scoped>
+	nav{
+		text-align: center;
+	}
+
+	nav ul{
+		padding: 0;
+	}
+	nav li{
+		display: inline-block;
+		list-style-type: none;
+		margin: 0;
+		padding: 8px;
+	}
+	
+</style>
