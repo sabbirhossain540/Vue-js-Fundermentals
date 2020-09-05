0ac8e27fae92b31f42314e8f90df5ea35d4eb8f4
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-05 13:13:39 +0600

    vue method

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ title }}</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      title : 'Wow Our View Website are Running'
+    }
   }
 }
+
 </script>
 
 <style>
