b754e1499da60528a9c9396b9db87556e03704cb
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-05 21:38:47 +0600

    axios request

--- vue-practice/src/components/Hook.vue
+++ vue-practice/src/components/Hook.vue
@@ -1,39 +1,48 @@
 <template>
 	<div class="Hook">
-		<h1>{{ title }}</h1>
-		<button @click="ChangeTitle">Click Me</button>
+		<h1>Posts</h1><hr>	
+		<div v-for="post in posts" :key="post.id">
+			<h2>{{ post.title }}</h2>
+			<p>{{ post.body }}</p>			
+		</div>
+
+
 	</div>
 	
 </template>
 
 <script>
-
+	import axios from 'axios'
 	export default {
 		name: 'Hook',
 		data(){
 			return {
-				title: 'Hook main data'
+				posts:[]
 			}
 		},
 		methods:{
-			ChangeTitle(){
-				this.title = 'Hook Updated Data'
-			}
-		},
-		beforeCreate(){
-				alert("Before Accelarated Hook")
+	
 		},
 		created(){
-				alert("Created Hook")
-		},
-		beforeUpdate(){
-			alert("Before update Accelarated Hook")
+			axios.get('https://jsonplaceholder.typicode.com/posts')
+			.then(response =>{
+				console.log(response)
+				this.posts = response.data
+			})
+			.catch(error=>{
+				console.log(error)
+			})
 		}
 	}
 
 </script>
 
 <style scoped>
-
+h1{
+	color: red;text-align: center;
+}
+table tr td {
+	border-right: 1px solid black;
+}
 	
 </style>
